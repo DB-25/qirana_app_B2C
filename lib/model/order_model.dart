491c295a957c78ce8c2c68e9d52ddf07bba6c446
wasiarsr <wasi@arsrgroup.com>
2020-07-28 12:38:21 +0530

    models added

---
+++ lib/model/order_model.dart
@@ -0,0 +1,34 @@
+class OrderModel {
+  String primaryId;
+  String buyingDate;
+  String item;
+  String quantity;
+  String weight;
+  String price;
+  String mrp;
+  String buyerName;
+
+  OrderModel(
+      {this.primaryId,
+      this.buyingDate,
+      this.item,
+      this.quantity,
+      this.weight,
+      this.price,
+      this.mrp,
+      this.buyerName});
+
+  factory OrderModel.fromMap(Map<String, dynamic> map) {
+    return OrderModel(
+      primaryId: map.containsKey('primaryId') ? (map['primaryId'] ?? '') : '',
+      buyingDate:
+          map.containsKey('buyingDate') ? (map['buyingDate'] ?? '') : '',
+      item: map.containsKey('item') ? (map['item'] ?? '') : '',
+      quantity: map.containsKey('quantity') ? (map['quantity'] ?? '') : '',
+      weight: map.containsKey('weight') ? (map['weight'] ?? '') : '',
+      price: map.containsKey('price') ? (map['price'] ?? '') : '',
+      mrp: map.containsKey('mrp') ? (map['mrp'] ?? '') : '',
+      buyerName: map.containsKey('buyerName') ? (map['buyerName'] ?? '') : '',
+    );
+  }
+}
